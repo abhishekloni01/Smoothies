3404204ed277f24c97a550f2e71502c01b64dd7c
Abhishek A Loni <61189274+abhishekloni01@users.noreply.github.com>
2024-06-27 21:45:48 +0530

    Removed a dataframe display

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ session = cnx.session()
 #session = get_active_session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True);
+#st.dataframe(data=my_dataframe, use_container_width=True);
 
 
 ingredients_list = st.multiselect(
