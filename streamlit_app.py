65a0b2c6a3f84eca6dde5437373a226a661f1a95
Abhishek A Loni <61189274+abhishekloni01@users.noreply.github.com>
2024-06-27 22:19:28 +0530

    Updated API to fetch selected fruits info

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string);
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
     
     my_insert_stmt = """insert into smoothies.public.orders(ingredients,name_on_order) values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     #st.write(my_insert_stmt)
@@ -39,9 +41,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon = '✅')
 
 
-# New section to display fruityvice nutrition information
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/mango")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
+
 
 
